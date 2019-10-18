92ccbaf4d004ad37328916fb4da61149e44ebe52
Krishan Nattar <krishan.nattar@gmail.com>
2019-10-18 08:41:59 -0700

    working on models

--- java-starthere-master/StartHere/src/main/java/com/lambdaschool/starthere/models/Book.java
+++ java-starthere-master/StartHere/src/main/java/com/lambdaschool/starthere/models/Book.java
@@ -1,4 +1,101 @@
 package com.lambdaschool.starthere.models;
 
-public class Book {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.lambdaschool.starthere.logging.Loggable;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Loggable
+@Entity
+@Table(name = "book")
+public class Book extends Auditable {
+
+    @ApiModelProperty(name = "bookid", value = "primary key for a book", required = true, example = "1")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long bookid;
+
+    @ApiModelProperty(name = "title", value = "Title of Book", example = "The War of Art")
+    @Column(nullable = false)
+    private String title;
+
+    @ApiModelProperty(name = "ISBN", value = "ISBN Number", example = "978-3-16-148410-0")
+    @Column(nullable = false, unique = true)
+    private String ISBN;
+
+    @ApiModelProperty(name = "copy", value = "Year the book was published", example = "2011")
+    private int copy;
+
+    @OneToMany(mappedBy = "book",
+            cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("book")
+    private List<Wrote> wrote = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "sectionid")
+    @JsonIgnoreProperties("books")
+    private Section section;
+
+    public Book() {
+
+    }
+
+    public Book(String title, String ISBN, Integer copy, Section section) {
+        this.title = title;
+        this.ISBN = ISBN;
+        this.copy = copy;
+        this.section = section;
+    }
+
+    public long getBookid() {
+        return bookid;
+    }
+
+    public void setBookid(long bookid) {
+        this.bookid = bookid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getCopy() {
+        return copy;
+    }
+
+    public void setCopy(Integer copy) {
+        this.copy = copy;
+    }
+
+    public List<Wrote> getwrote() {
+        return wrote;
+    }
+
+    public void setwrote(List<Wrote> wrote) {
+        this.wrote = wrote;
+    }
+
+    public Section getSection() {
+        return section;
+    }
+
+    public void setSection(Section section) {
+        this.section = section;
+    }
+
 }
--- java-starthere-master/StartHere/src/main/java/com/lambdaschool/starthere/models/Section.java
+++ java-starthere-master/StartHere/src/main/java/com/lambdaschool/starthere/models/Section.java
@@ -1,4 +1,65 @@
 package com.lambdaschool.starthere.models;
 
-public class Section {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "section")
+public class Section extends Auditable {
+
+    @ApiModelProperty(name = "sectionid", value = "primary key for a section", required = true, example = "1")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long sectionid;
+
+    @ApiModelProperty(name = "name", value = "Name of section", example = "Science Fiction")
+    @Column(nullable = false,
+            unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "section")
+    @JsonIgnoreProperties("sections")
+    private List<Book> books = new ArrayList<>();
+
+    public Section() {
+
+    }
+
+    public Section(String name) {
+        this.name = name;
+//        this.books = books;
+    }
+
+    public Section(String name, List<Book> books) {
+        this.name = name;
+        this.books = books;
+    }
+
+    public long getSectionid() {
+        return sectionid;
+    }
+
+    public void setSectionid(long sectionid) {
+        this.sectionid = sectionid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
 }

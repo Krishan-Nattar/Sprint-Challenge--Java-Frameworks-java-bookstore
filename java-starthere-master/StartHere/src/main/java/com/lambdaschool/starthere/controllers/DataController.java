cd1572b5ab3354f1b07d25dc773108ad3384801f
Krishan Nattar <krishan.nattar@gmail.com>
2019-10-18 08:36:08 -0700

    building out controllers

--- java-starthere-master/StartHere/src/main/java/com/lambdaschool/starthere/controllers/DataController.java
+++ java-starthere-master/StartHere/src/main/java/com/lambdaschool/starthere/controllers/DataController.java
@@ -1,4 +1,71 @@
 package com.lambdaschool.starthere.controllers;
 
+import com.lambdaschool.starthere.models.Book;
+import com.lambdaschool.starthere.models.ErrorDetail;
+import com.lambdaschool.starthere.services.BookService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.URISyntaxException;
+
+@RestController
+@RequestMapping(value = "/data")
 public class DataController {
+
+    @Autowired
+    private BookService bookService;
+
+    @ApiOperation(value = "Updates book details", response = void.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Book updated successfully", response = void.class),
+            @ApiResponse(code = 500, message = "Error updating book", response = ErrorDetail.class)
+    })
+    @PutMapping(value = "/books/{id}")
+    public ResponseEntity<?> updateStudent(
+            @RequestBody
+                    Book updateBook,
+            @PathVariable
+                    long id, HttpServletRequest request)
+    {
+        bookService.update(updateBook, id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Assign a book to an author", response = void.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Book assigned Successfully", response = void.class),
+            @ApiResponse(code = 500, message = "Error assigning book", response = ErrorDetail.class)
+    })
+    @PostMapping(value = "/books/{bookid}/authors/{authorid}",
+            consumes = {"application/json"},
+            produces = {"application/json"})
+    public ResponseEntity<?> assignBookToAuthor(@ApiParam(value = "Book Id", required = true, example = "1")
+                                                @PathVariable long bookid, @ApiParam(value = "Author Id", required = true, example = "1")
+                                                @PathVariable long authorid, HttpServletRequest request) throws URISyntaxException
+    {
+        bookService.assignBookToAuthor(bookid, authorid);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Delete a book", notes = "The book id entered will be deleted", response = void.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Book deleted successfully", response = void.class),
+            @ApiResponse(code = 500, message = "Error deleting book", response = ErrorDetail.class)
+    })
+    @DeleteMapping("/books/{bookid}")
+    public ResponseEntity<?> deleteBookById(@ApiParam(value = "Book Id", required = true, example = "1")
+                                            @PathVariable long bookid, HttpServletRequest request)
+    {
+        bookService.delete(bookid);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
 }

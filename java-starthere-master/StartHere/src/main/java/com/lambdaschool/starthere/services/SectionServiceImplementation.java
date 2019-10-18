c33b8cb1c4aed5de1ef46bc27afcc41c2fbba80c
Krishan Nattar <krishan.nattar@gmail.com>
2019-10-18 09:08:41 -0700

    sectionservice and authorservice use the same findall code? lets find out

--- java-starthere-master/StartHere/src/main/java/com/lambdaschool/starthere/services/SectionServiceImplementation.java
+++ java-starthere-master/StartHere/src/main/java/com/lambdaschool/starthere/services/SectionServiceImplementation.java
@@ -1,4 +1,24 @@
 package com.lambdaschool.starthere.services;
 
-public class SectionServiceImplementation {
+import com.lambdaschool.starthere.models.Book;
+import com.lambdaschool.starthere.models.Section;
+import com.lambdaschool.starthere.repository.SectionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service(value = "sectionService")
+public class SectionServiceImplementation implements SectionService {
+
+    @Autowired
+    private SectionRepository sectionRepos;
+
+    @Override
+    public ArrayList<Section> findAll(Pageable pageable) {
+        ArrayList<Section> list = new ArrayList<>();
+        sectionRepos.findAll(pageable).iterator().forEachRemaining(list::add);
+        return list;
+    }
 }
